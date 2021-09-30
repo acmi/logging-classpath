20c0a98d9b38951c15340b3ffb135c2ac2397498
Aleksei Morozov <aleksei_morozov@epam.com>
2021-09-30 17:22:54 +0400

    Added log4j2

--- src/main/java/ru/dexsys/logging/Main.java
+++ src/main/java/ru/dexsys/logging/Main.java
@@ -3,11 +3,11 @@ package ru.dexsys.logging;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 import java.io.ByteArrayInputStream;
@@ -16,11 +16,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.setProperty("org.apache.poi.util.POILogger", "org.apache.poi.util.SLF4JLogger");
-
         var resultFile = new File(args.length > 0 ? args[1] : "out.docx");
 
         byte[] pictureData = null;
@@ -29,11 +27,11 @@ public class Main {
             var response = httpClient.execute(new HttpGet("https://cataas.com/cat/says/I%20love%20Java"));
             pictureData = EntityUtils.toByteArray(response.getEntity());
         } catch (IOException e) {
-            log.error("Couldn't download image", e);
+            LOGGER.error("Couldn't download image", e);
             System.exit(1);
         }
 
-        log.info("Picture downloaded, size={}", pictureData.length);
+        LOGGER.info("Picture downloaded, size={}", pictureData.length);
 
         try (var document = new XWPFDocument();
              var output = new FileOutputStream(resultFile)) {
@@ -44,19 +42,19 @@ public class Main {
 
             document.write(output);
         } catch (InvalidFormatException e) {
-            log.error("Couldn't add picture", e);
+            LOGGER.error("Couldn't add picture", e);
             System.exit(1);
         } catch (IOException e) {
-            log.error("Couldn't save document", e);
+            LOGGER.error("Couldn't save document", e);
             System.exit(1);
         }
 
-        log.info("Result file {} saved, opening...", resultFile);
+        LOGGER.info("Result file {} saved, opening...", resultFile);
 
         try {
             Desktop.getDesktop().open(resultFile);
         } catch (IOException e) {
-            log.error("Couldn't open {} in default viewer", resultFile, e);
+            LOGGER.error("Couldn't open {} in default viewer", resultFile, e);
         }
     }
 }
