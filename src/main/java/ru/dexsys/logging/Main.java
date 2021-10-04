47f36f97187cefd0bad7ff02e2697e5025aa4264
filip <filippov.alex1999@yandex.ru>
2021-10-04 16:42:01 +0400

    main+pom

--- src/main/java/ru/dexsys/logging/Main.java
+++ src/main/java/ru/dexsys/logging/Main.java
@@ -6,8 +6,8 @@ import org.apache.http.util.EntityUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.awt.*;
 import java.io.ByteArrayInputStream;
@@ -16,15 +16,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final Logger log = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.setProperty("org.apache.poi.util.POILogger", "org.apache.poi.util.SLF4JLogger");
+
 
         var resultFile = new File(args.length > 0 ? args[1] : "out.docx");
 
         byte[] pictureData = null;
-
         try (var httpClient = HttpClients.createDefault()) {
             var response = httpClient.execute(new HttpGet("https://cataas.com/cat/says/I%20love%20Java"));
             pictureData = EntityUtils.toByteArray(response.getEntity());
